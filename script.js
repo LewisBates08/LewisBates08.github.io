57045e2e8ba003c98802d65d14517384c5b78ea6
Lewis Bates <lewisbates2008@outlook.com>
2026-06-13 15:13:28 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.lucide) {
+    window.lucide.createIcons({
+      attrs: {
+        "stroke-width": 2,
+        "aria-hidden": "true"
+      }
+    });
+  }
+});
